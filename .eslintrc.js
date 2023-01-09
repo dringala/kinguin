89dd0b65255c714416afcc9b552861f363eaa7dd
Alex Dring <hello@alexdring.com>
2023-01-09 21:33:46 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true,
+		node: true
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: 'module'
+	},
+	rules: {
+		'no-console': 'off',
+		'no-unused-vars': 'off',
+		'no-constant-condition': 'off'
+	}
+};
